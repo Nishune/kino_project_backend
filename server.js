e16b139c514e3151d893d5d65270ba26d60ce8c6
Rikard Engström <rikardengstrom89@gmail.com>
2025-01-15 12:52:20 +0100

    Added EJS as template engine, and made a test to see if it works properly.

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 //importerar express för att skapa webbserver
 import express from 'express';
-
+import ejs from 'ejs';
 //skapar en ny express applikation/server i app.
 const app = express();
 
-//Express servar statiska filer från dist mappen
-//Om något letar efter styles.css så letar express i dist/styles.css
+//EJS config
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+//Static files from dist folder
 app.use(express.static('dist'));
 
-//hanterar alla andra vägar som inte matchade med en faktiskt fil
-//Skickar index.html som svar så att frontend-kod kan hantera routing
-app.get('*', (req, res) => {
-  res.sendFile('dist/index.html', { root: '.' });
+app.get('/test', (req, res) => {
+  res.render('test', { title: 'Test page ' });
 });
 
-//Startar servern på port 3080 och cl ett meddelande då det är igång.
-app.listen(3080, () => {
-  console.log('Server running on port 3080');
+app.listen(5080, () => {
+  console.log('Server running on port 5080');
 });
