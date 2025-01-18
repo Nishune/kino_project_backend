5cb8a4fb626a3c86bd5869a2f308a02b07842fb2
Rikard Engström <rikardengstrom89@gmail.com>
2025-01-18 22:06:29 +0100

    Removed unnescessery files since json is replaces by js data files. removed about.js since its not genereted in the backend. Before it was all genereted from JS on the client. It works as intended right now, but it can probably be fixed even better. This will be done in another branch. Also removed folder that held the previously used json files.

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 import './style.scss';
 import './src/javascript/header.js';
 import './src/javascript/infoModal.js';
-
-// if (document.querySelector('.info') || document.querySelector('.info-modal')) {
-//   buildDoc();
-// }
