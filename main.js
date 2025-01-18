b3f728f4064f9233f21459502fb2a9a5fbaf59d6
Rikard Engström <rikardengstrom89@gmail.com>
2025-01-18 19:05:09 +0100

    Added new EJS to about.ejs file. Added the data to render for the about page in server.js. Removed unnecessery JS files for the about page. About page should now be fully setup to render on backend, there might be some minor things that needs work. But this will then be fixed in another branch.

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
 import './style.scss';
 import './src/javascript/header.js';
-// import { loadkids } from './src/javascript/kids.js';
 import { buildDoc } from './src/javascript/infoModal.js';
-import { updateDomWithAboutJson } from './src/javascript/about.js';
 
-// const checkKids = document.querySelector('.article-kids');
-
-// if (checkKids) {
-//   loadkids();
-// }
-
-// const checkMovies = document.querySelector('.movie-container');
-
-// if (checkMovies) {
-//   loadMovieContent();
-// }
 if (document.querySelector('.info') || document.querySelector('.info-modal')) {
   buildDoc();
 }
