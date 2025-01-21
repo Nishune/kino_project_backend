2fd569c4b274f89208b3f19bff9135b3727fe8c5
Rikard Engström <rikardengstrom89@gmail.com>
2025-01-21 09:02:01 +0100

    Made a first test as shown on our lectures.

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -1,6 +1,11 @@
 import { expect, test } from '@jest/globals';
 import app from '../src/server/app.js';
 import request from 'supertest';
+
 test('Home page shows a list of movies', async () => {
-  await request(app).get('/').expect('Content-Type', /html/).expect(200);
+  const response = await request(app).get('/movies').expect('Content-Type', /html/).expect(200);
+
+  expect(response.text).toMatch('Encanto');
+  expect(response.text).toMatch('Forrest Gump');
+  expect(response.text).toMatch('Training Day');
 });
