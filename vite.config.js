bc376b6d7adfc690fcc5feb2d9d051c6d14d8bfc
Rikard Engström <rikardengstrom89@gmail.com>
2025-01-23 14:55:20 +0100

    Removed some previously used code from vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,3 @@
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-//   base: '/',
-//   build: {
-//     outDir: 'dist',
-//     rollupOptions: {
-//       input: './main.js',
-//       output: {
-//         entryFileNames: `[name].js`,
-//         chunkFileNames: `[name].js`,
-//         assetFileNames: `[name].[ext]`,
-//       },
-//     },
-//   },
-//   css: {
-//     devSourcemap: true,
-//   },
-//   publicDir: 'public',
-// });
-
 import { defineConfig } from 'vite';
 
 export default defineConfig({
