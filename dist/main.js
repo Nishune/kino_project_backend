c78847cae6d9b2628258997b5bdb02ef3665d903
Rikard Engström <rikardengstrom89@gmail.com>
2025-01-23 14:40:42 +0100

    Added comments to header.js.

--- dist/main.js
+++ dist/main.js
@@ -1,28 +1,31 @@
-const t = document.querySelector('.hamburger-btn'),
-  l = document.querySelector('.close-btn'),
-  a = document.querySelector('.menu-overlay'),
-  s = document.querySelector('.overlay-blur');
-t == null ||
-  t.addEventListener('click', () => {
-    a.classList.add('active'), s.classList.add('active');
-  });
-l == null ||
-  l.addEventListener('click', () => {
-    a.classList.remove('active'), s.classList.remove('active');
-  });
-s == null ||
-  s.addEventListener('click', () => {
-    a.classList.remove('active'), s.classList.remove('active');
-  });
-const i = document.querySelectorAll('.modal-open');
-document.querySelectorAll('.modal-question');
-const r = (e, c) => {
-  e.classList.toggle('open-button-clicked'),
-    e.classList.contains('open-button-clicked')
-      ? ((e.src = e.dataset.closeSrc), (e.alt = e.dataset.closeAlt), c.classList.add('active'))
-      : ((e.src = e.dataset.openSrc), (e.alt = e.dataset.openAlt), c.classList.remove('active'));
-};
+const r = document.querySelector('.hamburger-btn'),
+  n = document.querySelector('.close-btn'),
+  l = document.querySelector('.menu-overlay'),
+  t = document.querySelector('.overlay-blur');
+r.addEventListener('click', () => {
+  l.classList.add('active'), t.classList.add('active');
+});
+n.addEventListener('click', () => {
+  l.classList.remove('active'), t.classList.remove('active');
+});
+t.addEventListener('click', () => {
+  l.classList.remove('active'), t.classList.remove('active');
+});
+const i = document.querySelectorAll('.modal-open'),
+  d = document.querySelectorAll('.modal-question'),
+  a = (e, s) => {
+    e.classList.toggle('open-button-clicked'),
+      e.classList.contains('open-button-clicked')
+        ? ((e.src = e.dataset.closeSrc), (e.alt = e.dataset.closeAlt), s.classList.add('active'))
+        : ((e.src = e.dataset.openSrc), (e.alt = e.dataset.openAlt), s.classList.remove('active'));
+  };
 i.forEach((e) => {
-  const o = e.closest('li').querySelector('.modal-answer');
-  e.addEventListener('click', () => r(e, o));
+  const c = e.closest('li').querySelector('.modal-answer');
+  e.addEventListener('click', () => a(e, c));
+});
+d.forEach((e) => {
+  const s = e.closest('li'),
+    c = s.querySelector('.modal-open'),
+    o = s.querySelector('.modal-answer');
+  e.addEventListener('click', () => a(c, o));
 });
--- src/javascript/header.js
+++ src/javascript/header.js
@@ -3,17 +3,29 @@ const closeBtn = document.querySelector('.close-btn');
 const menuOverlay = document.querySelector('.menu-overlay');
 const overlayBlur = document.querySelector('.overlay-blur');
 
-hamburgerBtn?.addEventListener('click', () => {
+// ========================================
+// Open the mobile menu
+//=========================================
+
+hamburgerBtn.addEventListener('click', () => {
   menuOverlay.classList.add('active');
   overlayBlur.classList.add('active');
 });
 
-closeBtn?.addEventListener('click', () => {
+// ========================================
+// Closing the mobile menu
+//=========================================
+
+closeBtn.addEventListener('click', () => {
   menuOverlay.classList.remove('active');
   overlayBlur.classList.remove('active');
 });
 
-overlayBlur?.addEventListener('click', () => {
+// ========================================
+// Close mobile menu when clicking outside the overlay, on the blur.
+//=========================================
+
+overlayBlur.addEventListener('click', () => {
   menuOverlay.classList.remove('active');
   overlayBlur.classList.remove('active');
 });
